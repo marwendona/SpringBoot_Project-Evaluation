ad9fc09b9edeb8b880b7d9f64402bc3cc9ab585a
Marwen Kammoun <marwenkammoun3@gmail.com>
2023-08-30 03:36:00 +0100

    add Repositories

---
+++ BackEnd/Evaluation/src/main/java/tn/primatec/evaluation/dao/StabilityRepository.java
@@ -0,0 +1,9 @@
+package tn.primatec.evaluation.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import tn.primatec.evaluation.dto.EmployeeDto;
+
+@RepositoryRestResource
+public interface StabilityRepository extends JpaRepository<EmployeeDto, Long> {
+}
